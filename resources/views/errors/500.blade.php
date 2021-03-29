86cd17927043ecf7cc5ef29a8bfbef8eb0b91e9c
Lamine <papalammine1.sane@uvs.edu.sn>
2021-03-29 11:23:39 +0100

    Error page setted

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description"
+        content="Robust admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template.">
+    <meta name="keywords"
+        content="admin template, robust admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="PIXINVENT">
+    <title>Error 500 - App UVS</title>
+    <link rel="apple-touch-icon" href="{{ asset('back-robust/app-assets/images/ico/apple-icon-120.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CMuli:300,400,500,700"
+        rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('back-robust/app-assets/css/vendors.css') }}">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN ROBUST CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('back-robust/app-assets/css/app.css') }}">
+    <!-- END ROBUST CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('back-robust/app-assets/css/core/menu/menu-types/horizontal-menu.css') }}">
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('back-robust/app-assets/css/core/colors/palette-gradient.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('back-robust/app-assets/css/pages/error.css') }}">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('back-robust/assets/css/style.css') }}">
+    <!-- END Custom CSS-->
+</head>
+
+<body class="horizontal-layout horizontal-menu horizontal-menu-padding 1-column   menu-expanded blank-page blank-page"
+    data-open="hover" data-menu="horizontal-menu" data-col="1-column">
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <section class="flexbox-container">
+                    <div class="col-12 d-flex align-items-center justify-content-center">
+                        <div class="col-md-4 col-10 p-0">
+                            <div class="card-header bg-transparent border-0">
+                                <h2 class="error-code text-center mb-2">500</h2>
+                                <h3 class="text-uppercase text-center">Internal Server Error</h3>
+                            </div>
+                            <div class="card-content">
+                                <div class="row py-2">
+                                    <div class="col-12">
+                                        <a href="/" class="btn btn-primary btn-block"><i class="ft-home"></i>
+                                            Accueil</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-transparent">
+                                <div class="row">
+                                    <p class="text-muted text-center col-12 py-1">© 2021 <a href="#">App </a>réalisée par la<a
+                                            href="#"
+                                            target="_blank">DISI</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+    </div>
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+
+    <!-- BEGIN VENDOR JS-->
+    <script src="{{ asset('back-robust/app-assets/vendors/js/vendors.min.js') }}"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->
+    <script src="{{ asset('back-robust/app-assets/vendors/js/ui/jquery.sticky.js') }}"></script>
+    <script src="{{ asset('back-robust/app-assets/vendors/js/forms/validation/jqBootstrapValidation.js') }}">
+    </script>
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN ROBUST JS-->
+    <script src="{{ asset('back-robust/app-assets/js/core/app-menu.js') }}"></script>
+    <script src="{{ asset('back-robust/app-assets/js/core/app.js') }}"></script>
+    <!-- END ROBUST JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="{{ asset('back-robust/app-assets/js/scripts/forms/form-login-register.js') }}"></script>
+    <!-- END PAGE LEVEL JS-->
+</body>
+
+</html>
